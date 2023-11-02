e5f801de342ff1bcfdb4563c25ecfc5e5c0e1994
mr_rashad <mr_rashad@email.com>
2023-11-02 23:20:57 +0400

    add

--- src/main/java/com/company/service/BookService.java
+++ src/main/java/com/company/service/BookService.java
@@ -1,8 +1,57 @@
 package com.company.service;
 
+import com.company.dao.BookRepository;
+import com.company.entity.Book;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import javax.swing.plaf.SpinnerUI;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
+    private final BookRepository bookRepository;
 
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getOneBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateOneBookById(Long id, Book newBook) {
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            Book foundBook = new Book();
+            foundBook.setName(newBook.getName());
+            foundBook.setAuthor(newBook.getAuthor());
+            bookRepository.save(foundBook);
+            return foundBook;
+        } else
+            return null;
+    }
+
+    public void deleteOneBookById(Long id) {
+        try {
+            bookRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            System.out.println("Book " + id + " doesn't exist");
+        }
+    }
+
+    public Book getOneBookByName(String name) {
+        return bookRepository.findByName(name);
+    }
 }
+
+
