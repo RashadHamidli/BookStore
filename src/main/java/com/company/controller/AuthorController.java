cc0acb10de8a7cc7c49edd377108bb9bd877b026
mr_rashad <mr_rashad@email.com>
2023-11-03 00:21:36 +0400

    add

--- src/main/java/com/company/controller/AuthorController.java
+++ src/main/java/com/company/controller/AuthorController.java
@@ -1,7 +1,34 @@
 package com.company.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.dto.AuthorDTO;
+import com.company.entity.Author;
+import com.company.service.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Author>> getAllAuthors() {
+        List<Author> authors = authorService.getAllAuthors();
+        return ResponseEntity.ok(authors);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> getOneAuthor(@PathVariable Long id) {
+        Author author = authorService.getOneAuthorById(id);
+        return ResponseEntity.ok(author);
+    }
+    @PostMapping()
+    public ResponseEntity<Author> createOneAuthor(@RequestBody AuthorDTO authorDTO){
+        return null;
+    }
 }
