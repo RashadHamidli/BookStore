e885769f3f879b71c816ab5fd8a76e02a85fa245
mr_rashad <mr_rashad@email.com>
2023-11-02 18:24:51 +0400

    add

--- src/main/java/com/company/dao/BookRepository.java
+++ src/main/java/com/company/dao/BookRepository.java
@@ -2,6 +2,8 @@ package com.company.dao;
 
 import com.company.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
