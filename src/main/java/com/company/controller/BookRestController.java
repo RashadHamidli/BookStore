37332f1e86798627629ded73ed9024a3498ffa79
mr_rashad <mr_rashad@email.com>
2023-11-05 19:34:32 +0400

    add

--- src/main/java/com/company/controller/BookRestController.java
+++ src/main/java/com/company/controller/BookRestController.java
@@ -1,12 +1,11 @@
 package com.company.controller;
 
+import com.company.dto.AuthorDTO;
 import com.company.dto.BookDTO;
+import com.company.service.AuthorService;
 import com.company.service.BookService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
@@ -21,5 +20,11 @@ public class BookRestController {
     public ResponseEntity<BookDTO> creatBook(@RequestBody BookDTO newBookDTO) {
         BookDTO bookDTO = bookService.creatBook(newBookDTO);
         return ResponseEntity.ok(bookDTO);
+    }
+
+    @PostMapping("author/{id}")
+    public ResponseEntity<BookDTO> creatBookByAuthorId(@PathVariable Long id, @RequestBody BookDTO newBookDTO) {
+            BookDTO bookDTO = bookService.creatBookByAuthorId(id, newBookDTO);
+            return ResponseEntity.ok(bookDTO);
     }
 }
--- src/main/java/com/company/service/BookService.java
+++ src/main/java/com/company/service/BookService.java
@@ -1,6 +1,7 @@
 package com.company.service;
 
 import com.company.dao.BookRepository;
+import com.company.dto.AuthorDTO;
 import com.company.dto.BookDTO;
 import com.company.entity.Author;
 import com.company.entity.Book;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final AuthorService authorService;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, AuthorService authorService) {
         this.bookRepository = bookRepository;
+        this.authorService = authorService;
     }
 
     public BookDTO creatBook(BookDTO newBookDTO) {
@@ -19,7 +22,33 @@ public class BookService {
         Book saveBook = bookRepository.save(book);
         return convertToDTO(saveBook);
     }
+    //------------------------------------------------------------
+    public BookDTO creatBookByAuthorId(Long id, BookDTO newBookDTO) {
+        AuthorDTO authorDTO = authorService.getAuthor(id);
+        Author author = convertToEntity(authorDTO);
+        Book book = convertToBookEntity(author, newBookDTO);
+        Book saveBook = bookRepository.save(book);
+        return convertToDTO(saveBook);
+    }
 
+    private Book convertToBookEntity(Author author, BookDTO bookDTO) {
+        Book book = new Book();
+        book.setName(bookDTO.getName());
+        book.setAuthor(author);
+        return book;
+    }
+
+    private Author convertToEntity(AuthorDTO authorDTO) {
+        Author author = new Author();
+        author.setId(authorDTO.getId());
+        author.setName(authorDTO.getName());
+        author.setEmail(authorDTO.getEmail());
+        author.setAge(authorDTO.getAge());
+        author.setPassword(authorDTO.getPassword());
+        return author;
+    }
+
+    //------------------------------------------------------------
     private BookDTO convertToDTO(Book book) {
         BookDTO bookDTO = new BookDTO();
         bookDTO.setId(book.getId());
